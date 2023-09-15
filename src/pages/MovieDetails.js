ea0fd758e9c1c351908e8cc28a8d69000e5d2912
ebiapollos <doyinapollos@gmail.com>
2023-09-15 13:29:14 +0100

    mobile-responsive update

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -13,6 +13,7 @@ import playMovieIconWhite from "../assets/svgs/play-movie-icon-white.svg";
 import starIcon from "../assets/svgs/star-icon.svg";
 import movieTicketsIcon from "../assets/svgs/movie-tickets-icon.svg";
 import moreOptionsMenuIcon from "../assets/svgs/more-option-menu-icon.svg";
+import NavBarMobile from "../components/NavBarMobile";
 
 function MovieDetails() {
   const { Id } = useParams();
@@ -41,20 +42,22 @@ function MovieDetails() {
 
   if (!movie) {
     return (
-      <div className="spinner border-t-4 border-blue-500 border-solid h-12 w-12 rounded-full animate-spin flex justify-center items-center"></div>
+      <div className="flex justify-center items-center h-screen ">
+        <div className="spinner border-t-4 border-blue-500 border-solid h-12 w-12 rounded-full animate-spin flex justify-center items-center"></div>
+      </div>
     );
   }
 
   return (
     <div className="w-full h-auto flex justify-center items-start poppins ">
-      <div className="w-[20%] h-[100vh] rounded-tr-[30px] border border-solid border-gray-300 bg-white flex flex-col justify-between items-center ">
-        <div className=" w-[85%] h-[70px] bg-red-400 md:h-[15%] mt-[15px] cursor-pointer rounded-[5px] p-[15px] md:bg-gray-300 bg-opacity-20 flex items-center ">
+      <div className="hidden w-[20%] h-[100vh] rounded-tr-[30px] border border-solid border-gray-300 bg-white md:flex flex-col justify-between items-center ">
+        <div className=" w-[85%] mt-[15px] cursor-pointer rounded-[5px] p-[15px] flex justify-center items-center bg-gray-500 bg-opacity-20 h-[50px] md:h-[100px] ">
           <Link to="/">
             {" "}
             <img
               src={movieAppLogo}
               alt="Logo"
-              className="h-[50px] w-[100px] md:h-[50px] md:w-[186px] "
+              className="h-[100%] w-[70px] md:h-[100px] md:w-[100%] "
             />
           </Link>{" "}
         </div>{" "}
@@ -153,8 +156,11 @@ function MovieDetails() {
           <span className="cursor-pointer"> Logout </span>{" "}
         </div>{" "}
       </div>{" "}
-      <div className="w-[80%] h-[100vh] flex flex-col justify-start items-center ">
-        <div className="w-[95%] h-[300px] bg-image2 bg-opacity-50 rounded-[30px] mt-[15px] flex flex-col justify-center items-center ">
+      <div className="w-[100%] h-[100vh] md:w-[80%] flex flex-col justify-start items-center ">
+        <div className="w-[100%] h-[60px] bg-gray-700 opacity-50 md:hidden lg:hidden ">
+          <NavBarMobile />
+        </div>
+        <div className="h-[240px] w-[100%] md:w-[95%] mt-[0px] md:h-[300px] bg-image2 bg-opacity-50 md:rounded-[30px] md:mt-[15px] flex flex-col justify-center items-center ">
           <div>
             <img
               src={playMovieIconWhite}
@@ -164,21 +170,21 @@ function MovieDetails() {
           </div>{" "}
           <span className="text-white font-[600] "> Watch Trailer </span>{" "}
         </div>{" "}
-        <div className="w-[95%] h-[50px] my-[10px] flex justify-between items-center ">
-          <div className="w-[65%] h-[50px] flex justify-start items-center ">
+        <div className="w-[95%] h-[100px] flex-col md:h-[50px] md:flex md:my-[10px] md:flex-row justify-between items-center ">
+          <div className="w-[100%] h-[70%] flex-col justify-between text-[12px] py-[7px] md:py-[0px]  md:text-[13px] md:w-[65%] md:h-[50px] flex md:flex-row md:justify-start items-center ">
             <span> Top Gun: Maverick.2022.PG - 13.2 h 10 m </span>{" "}
-            <button class="w-[80px] h-[30px] mx-[7px] border border-solid border-[#BE123C] bg-white hover:bg-gray-700 rounded-[30px] flex justify-center items-center ">
+            <button class="w-[80px] h-[25px] md:h-[30px] mx-[7px] border border-solid border-[#BE123C] bg-white hover:bg-gray-700 rounded-[30px] flex justify-center items-center ">
               <span class="text-[#BE123C] text-[12px] font-[500] pl-[7px] ">
                 Action{" "}
               </span>{" "}
             </button>{" "}
-            <button class="w-[80px] h-[30px] border border-solid border-[#BE123C] bg-white hover:bg-gray-700 rounded-[30px] flex justify-center items-center ">
+            <button class="w-[80px] h-[25px] md:h-[30px] border border-solid border-[#BE123C] bg-white hover:bg-gray-700 rounded-[30px] flex justify-center items-center ">
               <span class="text-[#BE123C] text-[12px] font-[500] pl-[7px] ">
                 Drama{" "}
               </span>{" "}
             </button>{" "}
           </div>{" "}
-          <div className="w-[10%] h-full flex justify-center items-center ">
+          <div className="w-[100%] md:w-[10%]  flex justify-center items-center ">
             <img
               src={starIcon}
               alt="starIcon"
@@ -190,8 +196,8 @@ function MovieDetails() {
             </span>{" "}
           </div>{" "}
         </div>{" "}
-        <div className="w-[95%] h-[90px] flex justify-between items-center ">
-          <div className=" w-[100%] h-full flex justify-between items-center ">
+        <div className="w-[95%] h-[230px] md:h-[90px] flex justify-between items-center ">
+          <div className=" w-[100%] h-full flex-col flex md:flex-row justify-between items-center ">
             <div className="w-[75%] h-full  font-[400] flex justify-start items-start ">
               <p className="text-[14px]  ">
                 <span className="font-[600] "> Overview: </span>{" "}
@@ -201,8 +207,8 @@ function MovieDetails() {
                 </span>{" "}
               </p>{" "}
             </div>{" "}
-            <div className="w-[25%] h-full flex flex-col justify-center items-end ">
-              <button class="w-[220px] h-[30px] mx-[7px] mb-[10px] border border-solid border-[#BE123C] bg-[#BE123C] hover:bg-red-700 rounded-[5px] flex justify-center items-center ">
+            <div className="w-[100%] items-center md:w-[25%] md:h-full flex flex-col justify-center md:items-end ">
+              <button class="w-[200px] h-[30px] md:w-[220px] mx-[7px] mb-[10px] border border-solid border-[#BE123C] bg-[#BE123C] hover:bg-red-700 rounded-[5px] flex justify-center items-center ">
                 <img
                   src={movieTicketsIcon}
                   alt="homeLogo"
@@ -212,7 +218,7 @@ function MovieDetails() {
                   See Showtimes{" "}
                 </span>{" "}
               </button>{" "}
-              <button class="w-[220px] h-[30px] mx-[7px] border border-solid border-[#BE123C] bg-[#F8E7EB] hover:bg-red-700 rounded-[5px] flex justify-center items-center ">
+              <button class="w-[200px] h-[30px] md:w-[220px] mx-[7px] border border-solid border-[#BE123C] bg-[#F8E7EB] hover:bg-red-700 rounded-[5px] flex justify-center items-center ">
                 <img
                   src={moreOptionsMenuIcon}
                   alt="homeLogo"
@@ -225,10 +231,10 @@ function MovieDetails() {
             </div>{" "}
           </div>{" "}
         </div>{" "}
-        <div className="w-[95%] h-[130px] mt-[10px] border-t border-solid border-gray flex justify-between items-center ">
-          <div className="w-[75%] h-full text-[12px] flex flex-col justify-between items-start ">
+        <div className="w-[95%] h-[250px] flex-col md:h-[130px] mt-[10px] border-t border-solid border-gray flex md:flex-row justify-between items-center ">
+          <div className="w-[75%] h-[60%] mt-[7px] md:mt-[0px] md:w-[75%] md:h-full text-[12px] flex flex-col justify-between items-start ">
             <p>
-              Original title:{" "}
+              Title:{" "}
               <span className="" data-testid="movie-title">
                 {" "}
                 {movie.title}{" "}
@@ -248,8 +254,8 @@ function MovieDetails() {
                 {movie.vote_count}{" "}
               </span>{" "}
             </p>{" "}
-            <div className="w-[90%] h-[40px] border border-solid border-[#C7C7C7] flex justify-start items-center ">
-              <button class="w-[230px] h-[40px] border border-solid border-[#BE123C] bg-[#BE123C] hover:bg-red-700 rounded-[5px] flex justify-center items-center ">
+            <div className="w-[100%] h-[40%] border-none flex-col justify-between md:w-[661px] md:h-[40px] border md:border-solid border-[#C7C7C7] flex md:flex-row md:justify-start items-center ">
+              <button class="mb-[7px] w-[230px] h-[30px] md:h-[40px] md:mb-[0px] border border-solid border-[#BE123C] bg-[#BE123C] hover:bg-red-700 rounded-[5px] flex justify-center items-center ">
                 <span class="text-white text-[12px] font-[500] pl-[7px] ">
                   Top rated movies #65
                 </span>
@@ -257,7 +263,7 @@ function MovieDetails() {
               <div className="">
                 <select
                   id="mySelect"
-                  className="w-[430px] p-[7px] border shadow-md focus:ring focus:ring-blue-300"
+                  className="w-[230px] h-[30px] md:h-[40px] md:w-[430px] p-[7px] border shadow-md focus:ring focus:ring-blue-300"
                 >
                   <option value="option1"> Awards 9 nominations </option>{" "}
                   <option value="option2"> Option 2 </option>{" "}
@@ -266,38 +272,38 @@ function MovieDetails() {
               </div>{" "}
             </div>{" "}
           </div>{" "}
-          <div className="w-[25%] h-full flex flex-col justify-center items-center ">
-            <div className="w-full h-full flex justify-center items-center ">
+          <div className="w-[90%] md:w-[25%] md:h-full flex flex-col justify-center items-center ">
+            <div className=" w-full h-full flex justify-center items-center ">
               <div className="flex justify-center items-center ">
                 <img
                   src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                   alt="Logo"
-                  className="h-full w-full "
+                  className="w-[50px] h-[70px] md:h-full md:w-full "
                 />
               </div>{" "}
               <div className="border-l border-r border-white ">
                 <img
                   src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                   alt="Logo"
-                  className="h-full w-full "
+                  className="w-[50px] h-[70px] md:h-full md:w-full "
                 />
               </div>{" "}
               <div className=" ">
                 <img
                   src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                   alt="Logo"
-                  className="h-full w-full "
+                  className="w-[50px] h-[70px] md:h-full md:w-full "
                 />
               </div>{" "}
             </div>{" "}
-            <div className="w-[100%] h-[30px] ">
+            <div className=" hidden md:block w-[100%] h-[30px] ">
               <button class="w-[100%] h-[30px] border border-solid border-gray-900 bg-gray-700 bg-opacity-50 hover:bg-red-700 rounded-[4px] flex justify-center items-center ">
                 <img
                   src={moreOptionsMenuIcon}
                   alt="homeLogo"
                   className="h-[24px] w-[19px] mx-[7px] "
                 />
-                <span class="text-white text-[10px] font-[500] ">
+                <span class="text-white text-[9px] font-[500] ">
                   The best Movies and Shows in September{" "}
                 </span>{" "}
               </button>{" "}
